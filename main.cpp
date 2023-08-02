7b8ac6892439cbba32fe8946c13b968c511006a8
iblank01 <ianblankenship32@gmail.com>
2023-08-02 17:53:49 -0400

    Major change: implemented primitive but functional db where data can be inputted/read from to insert/display data, working on delete and login functionality to ensure data security and provide an 'undo' to users in the event of errant data-entry

--- main.cpp
+++ main.cpp
@@ -1,141 +1,71 @@
 #include <iostream>
-#include <string>
-#include <sstream>
+#include <sqlite3.h>
 #include <SQLiteCpp/SQLiteCpp.h>
-
-class RegistrationProcess
-{
-    public:
-        // easy to use functions for user to register, login, and exit, modularized
-        void userRegister();
-        void userLogin();
-        // may eventually add a userExit() function with a different functionality (i.e. return to home page, exiting the main menu screen
-//        void userExit();
-        void mainMenuSelect();
-};
-
-void RegistrationProcess::userRegister()
-{
-    // Create a database file, if it doesn't exist it will be created
-    SQLite::Database data_base("login_system.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
-    // Create a table if it doesn't exist
-    data_base.exec("CREATE TABLE IF NOT EXISTS users (username TEXT UNIQUE, password TEXT)");
-
-    std::string username;
-    std::string password;
-
-    // prompt user for username and password
-    std::cout << "Enter a username: ";
-    std::cin >> username;
-    std::cout << "Enter a password: ";
-    std::cin >> password;
-
-    // try to insert the username and password into the database
-    try
-    {
-        // The ? are placeholders for the values we will bind to the query, this is to prevent SQL injection
-        SQLite::Statement query(data_base, "INSERT INTO users VALUES (?, ?)");
-        query.bind(1, username);
-        query.bind(2, password);
-        query.exec();
-        std::cout << "User registered successfully\n";
-    }
-    // catch error if it is thrown in "try" block
-    catch (SQLite::Exception &e)
-    {
-        // If the username is already taken, SQLite will throw an exception, we catch it and print the error
-        std::cout << "Error: " << e.what() << '\n';
-    }
-}   // end userRegister
-
-
-void RegistrationProcess::userLogin()
-{
-    // Create a database file, if it doesn't exist it will be created
-    SQLite::Database data_base("login_system.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
-    // Create a table if it doesn't exist
-    data_base.exec("CREATE TABLE IF NOT EXISTS users (username TEXT UNIQUE, password TEXT)");
-
-    std::string username;
-    std::string password;
-
-    std::cout << "Enter your username: ";
-    std::cin >> username;
-    std::cout << "Enter your password: ";
-    std::cin >> password;
-
-    // Check if the username exists and if the password is correct
-    SQLite::Statement query(data_base, "SELECT password FROM users WHERE username = ?");
-    query.bind(1, username);
-
-    // If the query returns a row the username exists, else "No user with that username"
-    if (query.executeStep())
-    {
-        std::string stored_password = query.getColumn(0);
-
-        // if pass matches, login user, else, incorrect password
-        if (password == stored_password)
-        {
-            std::cout << "Login successful\n";
-        }
-        else
-        {
-            std::cout << "Incorrect username/password\n";
-        }
-    }
-    else
-    {
-        std::cout << "Incorrect username/password\n";
-    }
-}   // end userLogin
-
-
-void RegistrationProcess::mainMenuSelect()
-{
-    int main_menu_choice{ };
-    char repeat_choice{ };
-
-    // exe one time, then loop if user wants to try again
-    do
-    {
-        std::cout << "1) Register\n2) Login\n3) Exit\nEnter your main_menu_choice: ";
-        std::cin >> main_menu_choice;
-
-        // switch menu for simpler implementation of menu
-        switch (main_menu_choice)
-        {
-            case 1:
-            {
-                userRegister();
-                break;
-            }
-            case 2:
-            {
-                userLogin();
-                break;
-            }
-            case 3:
-            {
-                exit(-1);
-            }
-            default:
-                std::cout << "Invalid main_menu_choice\n";
-        }   // end switch
-
-        // ask user if they want to try again
-        std::cout << "Would you like to try again/return to main menu? (y/n): ";
-        std::cin >> repeat_choice;
-
-    } while (repeat_choice == 'y');
-}   // end mainMenuSelect
-
-
 
 
 int main()
 {
-    RegistrationProcess registration_process;
-    registration_process.mainMenuSelect();
+    std::cout << "**** To be turned into a test suite for SQLiteCpp and SQLite3 for other projects ****" << std::endl;
+    std::cout << '\n';
+
+    // create an object 'data_base' of class SQLite::Database and create a table called login-db if it does not already exist
+    SQLite::Database data_base("login-db.db", SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
+    data_base.exec("CREATE TABLE IF NOT EXISTS login-db (date INTEGER PRIMARY KEY, weight INTEGER, notes TEXT);");
+
+    // get user input for weight, notes, and date
+    std::cout << "Hello User! " << '\n' << "Please enter your weight: ";
+    int temp_weight;
+    std::cin >> temp_weight;
+
+    std::cout << "Please enter any notes you would like to add: ";
+    std::string temp_notes;
+    std::cin.ignore();
+    std::getline(std::cin, temp_notes);
+
+    std::cout << "Please enter the date in the format YYYYMMDD: ";
+    int temp_date;
+    std::cin >> temp_date;
+
+    // data insertion code
+    try
+    {
+        // query the database login-db and bind the date, weight, and notes that the user entered to the database
+        SQLite::Statement insert_user_stmt(data_base, "INSERT or IGNORE INTO login-db VALUES (?, ?, ?);");
+        insert_user_stmt.bind(1, temp_date);
+        insert_user_stmt.bind(2, temp_weight);
+        insert_user_stmt.bind(3, temp_notes);
+
+        if (insert_user_stmt.exec() == 0)  // Check if no rows were inserted
+        {
+            std::cout << "Data for the provided date already exists!" << std::endl;
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cout << "exception during insertion: " << e.what() << std::endl;
+    }
+
+    // display data code
+    try
+    {
+        // Compile a SQL query, containing one parameter (index 1)
+        SQLite::Statement query_stmt(data_base, "SELECT * FROM login-db");
+
+        // Loop to execute the query step by step, to get rows of result
+        while (query_stmt.executeStep())
+        {
+            // Demonstrate how to get some typed column value
+            int date = query_stmt.getColumn(0);
+            int weight = query_stmt.getColumn(1);
+            std::string notes = query_stmt.getColumn(2);
+
+            std::cout << "row: " << date << ", " << weight << ", " << notes << std::endl;
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cout << "exception during data display/query_stmt: " << e.what() << std::endl;
+    }
 
     return 0;
 }
